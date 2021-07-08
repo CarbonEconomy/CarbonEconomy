692e4d97b1b66ddea4d5d9946500f7c1bb973fd3
Tan Kang Liang <kang.tan@caperlab.com>
2021-07-08 20:02:19 +0800

    finish app bar

--- green_pouch/lib/my_colours.dart
+++ green_pouch/lib/my_colours.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class MyColours {
   static const Color PRIMARY = const Color(0xFF2DDA93);
+  static const Color APPBAR_TOP_LEFT = const Color(0xFF61D2C4);
+  static const Color APPBAR_BOTTOM_RIGHT = const Color(0xFF29D890);
 }
