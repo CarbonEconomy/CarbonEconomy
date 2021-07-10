f954b5f1c7d11222099574000ad1b71ef9295dba
tankangliang <tankangliang98@gmail.com>
2021-07-10 17:08:37 +0800

    add shopping section

--- green_pouch/lib/favourites/view.dart
+++ green_pouch/lib/favourites/view.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:green_pouch/favourites/widgets/fav_box.dart';
 
-
 class FavouritesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(15.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 20,
           ),
           Box(),
+          Container(
+            height: 20,
+          ),
+          Title("Shopping"),
         ],
       ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  String text;
+
+  Title(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0.7,
+      ),
+      textAlign: TextAlign.left,
     );
   }
 }
