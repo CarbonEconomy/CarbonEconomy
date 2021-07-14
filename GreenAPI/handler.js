72e3b09b1d6d78e097f44e42ed54d87e1e6ca703
simonteozw <simonteozw@gmail.com>
2021-07-14 17:41:47 +0800

    initial API setup

--- GreenAPI/handler.js
+++ GreenAPI/handler.js
@@ -1,6 +1,10 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const rewards = require('./routes/rewards.routes')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
@@ -14,6 +18,7 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+app.use('/', rewards)
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
---
+++ GreenAPI/routes/rewards.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const rewardsService = require('../services/rewards.service')
+
+router.get('/rewards', rewardsService.findAll)
+router.get('/rewards/:rewardId', rewardsService.findOne)
+
+module.exports = router
