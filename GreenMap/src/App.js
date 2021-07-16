659b43736bcf3966aaa4ee3035ed9a797915ad85
Ritesh Kumar <ritesh@emerald.pink>
2021-07-16 21:38:03 +0800

    Abstract Map Away Into MapContent

--- GreenMap/src/App.js
+++ GreenMap/src/App.js
@@ -3,29 +3,10 @@ import DeckGL, { MapController } from "deck.gl";
 import { renderLayers } from "./utils/RenderLayers";
 import {StaticMap}from "react-map-gl"
 
-import { csv } from "d3-fetch";
 import {INITIAL_VIEWPORT} from "./utils/MapUtils/Viewports";
-import {JURONG_HILL_} from "./utils/MapUtils/SingaporeLocations";
-import {fetchCsvData, fetchData} from "./dataLoaders/LocationsLoader"
+import {fetchCsvData} from "./dataLoaders/LocationsLoader"
 import {generateRandomGreenCredits} from "./utils/MockDataUtils/MockData";
-
-// const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_DEFAULT_PUBLIC_TOKEN
-//
-// console.log("XX token:", MAPBOX_ACCESS_TOKEN)
-
-function getTooltip({object}) {
-    if (!object) {
-        return null;
-    }
-    const lat = object.position[1];
-    const lng = object.position[0];
-    const count = object.points.length;
-
-    return `\
-    latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ''}
-    longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ''}
-    ${count} Accidents`;
-}
+import MapContent from "./pages/MapContent";
 
 const App = () => {
     const [data, setData] = useState(null);
@@ -66,22 +47,13 @@ const App = () => {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
-    const display =  <DeckGL
-        layers={renderLayers({
-            data: data
-        })}
-        controller={{ type: MapController, dragRotate: true }}
-        initialViewState={viewport}
-    >
-        <StaticMap
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            // mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
-            mapboxApiAccessToken={"pk.eyJ1IjoicnRzaGttciIsImEiOiJja3F3Z3RzOHMwMXgzMm9xdTU5ZTltMWk4In0.3QeM-It_jGjZKSB8BOcz_Q"} />
-    </DeckGL>
+    const layers = renderLayers({
+        data: data
+    })
 
     return (
         <div className="App">
-            {display}
+            <MapContent viewport={viewport} layers={layers}/>
         </div>
     );
 };
---
+++ GreenMap/src/pages/MapContent.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+import DeckGL, {MapController} from 'deck.gl';
+import {StaticMap} from 'react-map-gl';
+
+
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_DEFAULT_PUBLIC_TOKEN
+console.log("see me", MAPBOX_ACCESS_TOKEN)
+const JURONG_HILL = [1.3181935040627233, 103.70845164296772]
+const SELETAR_TOWER = [1.4030786091186036, 103.807320541631]
+const DATA_URL = "../../public/heatmap-data.csv"
+
+function MapContent(props) {
+    const {viewport, layers} = props
+
+    const display = <DeckGL
+        layers={layers}
+        controller={{type: MapController, dragRotate: true}}
+        initialViewState={viewport}
+    >
+        <StaticMap
+            mapStyle="mapbox://styles/mapbox/dark-v10"
+            mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
+
+    return display;
+
+}
+
+export default MapContent;
