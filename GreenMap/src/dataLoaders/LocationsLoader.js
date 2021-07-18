9e6d331e1df77242ad3a4894f0bd98994a55023b
Ritesh Kumar <ritesh@emerald.pink>
2021-07-18 22:55:20 +0800

    Minor Commit
    We all love promises, don't we?

--- GreenMap/src/dataLoaders/LocationsLoader.js
+++ GreenMap/src/dataLoaders/LocationsLoader.js
@@ -5,14 +5,23 @@ import {generateRandomTransactions} from "../utils/MockDataUtils/MockData";
 const SINGAPORE_LOCATIONS_DATA_URL = "/SingaporeLocations_.csv";
 
 export async function fetchTransactionsFlow() {
-    const csvContent = (await csv(SINGAPORE_LOCATIONS_DATA_URL)
+    return parseApiData(await csv(SINGAPORE_LOCATIONS_DATA_URL)
+        .then(csvContent => {
+            let mapped = csvContent.map(function (d) {
+                return {position: [+d.lng, +d.lat]};
+            })
+            return mapped
+        })
+        .then(mapped => {
+            return generateRandomTransactions(mapped, 1000)
+        })
         .catch(error => {
             console.log("we got an error boiss", error)
         }))
-        .map(function (d) {
-            return {position: [+d.lng, +d.lat]};
-        });
-    const transactions = await generateRandomTransactions(csvContent, 1000)
-    return parseApiData(transactions);
+    // .map(function (d) {
+    //     return {position: [+d.lng, +d.lat]};
+    // });
+    // const transactions = await generateRandomTransactions(csvContent, 1000)
+    // return parseApiData(transactions);
 }
 
