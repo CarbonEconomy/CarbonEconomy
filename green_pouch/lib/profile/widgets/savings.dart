96953baa407da01b0d68a9541296bed2603e868e
tankangliang <tankangliang98@gmail.com>
2021-07-18 18:50:58 +0800

    savings page

--- green_pouch/lib/profile/widgets/savings.dart
+++ green_pouch/lib/profile/widgets/savings.dart
@@ -10,8 +10,11 @@ class Savings extends StatelessWidget {
   ImageProvider<Object> buildImage(int progress) {
     if (progress <= 33) {
       return AssetImage("graphics/small.png");
+    } else if (progress <= 67) {
+      return AssetImage("graphics/med.png");
     }
-    return AssetImage("graphics/med.png");
+
+    return AssetImage("graphics/big.png");
   }
 
   @override
@@ -30,7 +33,65 @@ class Savings extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        Text("$progress")
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 240,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fitHeight,
+                image: buildImage(progress),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: MyColours.HEADER_TEXT_COLOUR),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: LinearProgressIndicator(
+                value: progress.toDouble() / 100,
+                backgroundColor: MyColours.PRIMARY.withOpacity(0.24),
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '100',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: MyColours.HEADER_TEXT_COLOUR),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(width: 10),
+            Container(
+                alignment: Alignment.topRight,
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Text(
+                  "${100 - progress} more to go",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      color: MyColours.HEADER_TEXT_COLOUR),
+                )),
+            SizedBox(width: 35),
+          ],
+        )
       ],
     );
   }
