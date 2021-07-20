3b2585dc07e04723856b2409c69fca0ecbbcf8b8
Samuel Fang <samuelfangjw@gmail.com>
2021-07-21 04:05:39 +0800

    Add dialog

--- GreenMap/src/layouts/TopMenu.js
+++ GreenMap/src/layouts/TopMenu.js
@@ -1,42 +1,107 @@
-import React from 'react'
-import {Avatar, Card, Grid, makeStyles, Typography, SvgIcon, Icon} from "@material-ui/core";
-import HelpIcon from '@material-ui/icons/Help';
-import zIndex from "@material-ui/core/styles/zIndex";
-import {colors} from "../utils/Colors"
-import CarbonEconomyLogo from './CarbonEconomy.svg';
-import x from './CarbonEconomy.svg'
+import React from "react";
+import { makeStyles, Icon } from "@material-ui/core";
+import { colors } from "../utils/Colors";
+import CarbonEconomyLogo from "./CarbonEconomy.svg";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles({
-    container: {
-        zIndex: "1000",
-    },
-    icon: {
-        zIndex: "1000",
-        backgroundColor: colors.pistachioGreen,
-        margin: "2%",
-        padding: "5%",
-    },
-    imageIcon: {
-        width: "150px",
-        height: '150px',
-        position: 'absolute',
-        top:0,
-        right:0,
-        zIndex:1000,
-    },
-    iconRoot: {
-        textAlign: 'center',
-        position: 'relative',
-        zIndex:1000,
-    }
+  container: {
+    zIndex: "1000",
+  },
+  icon: {
+    zIndex: "1000",
+    backgroundColor: colors.pistachioGreen,
+    margin: "2%",
+    padding: "5%",
+  },
+  imageIcon: {
+    width: "150px",
+    height: "150px",
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 1000,
+  },
+  iconRoot: {
+    textAlign: "center",
+    position: "relative",
+    zIndex: 1000,
+  },
+  closeButton: {
+    float: "right",
+    color: "black",
+    minWidth: 0,
+  },
+});
 
-})
+const dialogText =
+  "Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap! v Welcome to GreenMap! Welcome to GreenMap! Welcome to GreenMap!";
 
 export default function TopMenu() {
-    const classes = useStyles()
-    const imageDisplay = <Icon className={{root:classes.iconRoot}}>
-        <img className={classes.imageIcon} src={CarbonEconomyLogo} alt="x"/>
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const classes = useStyles();
+  const imageDisplay = (
+    <Icon className={{ root: classes.iconRoot }} onClick={handleClickOpen}>
+      <img className={classes.imageIcon} src={CarbonEconomyLogo} alt="x" />
     </Icon>
-    // return (<img src={x} alt="x"/>)
-    return imageDisplay
+  );
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  const dialog = (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      scroll="paper"
+      aria-labelledby="scroll-dialog-title"
+      aria-describedby="scroll-dialog-description"
+    >
+      <DialogTitle id="scroll-dialog-title">
+        GreenMap{" "}
+        <Button
+          className={classes.closeButton}
+          onClick={handleClose}
+          color="primary"
+        >
+          <CloseIcon />
+        </Button>
+      </DialogTitle>
+      <DialogContent dividers={scroll === "paper"}>
+        <DialogContentText
+          id="scroll-dialog-description"
+          ref={descriptionElementRef}
+          tabIndex={-1}
+        >
+          {dialogText}
+        </DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
+  return (
+    <>
+      {imageDisplay}
+      {dialog}
+    </>
+  );
 }
