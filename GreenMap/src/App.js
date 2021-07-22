be0d25c74e756b1646895a5d38e4b43381df4d31
Ritesh Kumar <ritesh@emerald.pink>
2021-07-22 22:54:02 +0800

    Shift notification's setInterval() into a useEffect
    This should allow us to add in a slider to let the user change the rate at which these toasts come in.

--- GreenMap/src/App.js
+++ GreenMap/src/App.js
@@ -1,167 +1,177 @@
-import React, { useEffect, useState } from "react";
-import { useLayers } from "./layers/UseLayers";
+import React, {useEffect, useRef, useState} from "react";
+import {useLayers} from "./layers/UseLayers";
 import {
-  createViewport,
-  INITIAL_VIEWPORT_CBD,
+    createViewport,
+    INITIAL_VIEWPORT_CBD,
 } from "./utils/MapUtils/Viewports";
-import { fetchTransactions } from "./dataLoaders/LocationsLoader";
+import {fetchTransactions} from "./dataLoaders/LocationsLoader";
 import MapContent from "./pages/MapContent";
 import LoadingPage from "./pages/LoadingPage";
 import TransactionNotification from "./components/TransactionNotification";
-import toast, { Toaster } from "react-hot-toast";
+import toast, {Toaster} from "react-hot-toast";
 import parseApiData from "./dataLoaders/ApiParser";
 import TopMenu from "./layouts/TopMenu";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import { makeStyles } from "@material-ui/core";
-import { borderColor, display } from "@material-ui/system";
+import {makeStyles} from "@material-ui/core";
+import {borderColor, display} from "@material-ui/system";
+import {colors} from "./utils/Colors"
 
 const useStyles = makeStyles({
-  checkbox: {
-    color: "white",
-    borderColor: "white",
-    margin: "0 !important"
-  },
-  checkboxes: {
-    display: "flex",
-    flexDirection: "column",
-    color: "white"
-  },
-  container: {
-    position: "absolute",
-    zIndex: "2000 !important",
-    top: 0,
-    right: 0,
-    display: "flex",
-    flexDirection: "column",
-  },
+    checkbox: {
+        color: "white",
+        borderColor: "white",
+        margin: "0 !important"
+    },
+    checkboxes: {
+        display: "flex",
+        flexDirection: "column",
+        color: "white"
+    },
+    container: {
+        position: "absolute",
+        zIndex: "2000 !important",
+        top: 0,
+        right: 0,
+        display: "flex",
+        flexDirection: "column",
+    },
 });
 
 const App = () => {
-  const [viewport, setViewport] = useState(INITIAL_VIEWPORT_CBD);
-  const [transactions, setTransactions] = useState([]);
-  const [transactionsFlow, setTransactionsFlow] = useState(null);
-  const [state, setState] = useState({
-    heatmap: false,
-    arcs: false,
-  });
-  const classes = useStyles();
-
-  const handleCheck = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
-
-  const handleResize = () => {
-    setViewport((v) => {
-      return {
-        ...v,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
+    const [viewport, setViewport] = useState(INITIAL_VIEWPORT_CBD);
+    const [transactions, setTransactions] = useState([]);
+    const [transactionsFlow, setTransactionsFlow] = useState(null);
+    const [tickPeriod, setUpdateInterval] = useState(2000) // updating of toasts
+    const [state, setState] = useState({
+        heatmap: false,
+        arcs: false,
     });
-  };
+    const classes = useStyles();
 
-  const handleViewportChange = (targetLocation) => {
-    const updatedViewport = createViewport(targetLocation);
-    console.log(">> updatedViewport:", updatedViewport);
-    setViewport(updatedViewport);
-  };
+    const handleCheck = (event) => {
+        setState({...state, [event.target.name]: event.target.checked});
+    };
 
-  async function fetchTransactionsData() {
-    await fetchTransactions().then((transactions) => {
-      setTransactions(transactions);
-      setTransactionsFlow(parseApiData(transactions));
+    const handleResize = () => {
+        setViewport((v) => {
+            return {
+                ...v,
+                width: window.innerWidth,
+                height: window.innerHeight,
+            };
+        });
+    };
+
+    const handleViewportChange = (targetLocation) => {
+        const updatedViewport = createViewport(targetLocation);
+        console.log(">> updatedViewport:", updatedViewport);
+        setViewport(updatedViewport);
+    };
+
+    async function fetchTransactionsData() {
+        await fetchTransactions().then((transactions) => {
+            setTransactions(transactions);
+            setTransactionsFlow(parseApiData(transactions));
+        });
+    }
+
+    useEffect(() => {
+        fetchTransactionsData();
+    }, []);
+
+
+    //resize
+    useEffect(() => {
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const layers = useLayers({
+        transactionsFlow: transactionsFlow,
+        enabled: state,
     });
-  }
 
-  useEffect(() => {
-    fetchTransactionsData();
-  }, []);
+    const transactionsRef = useRef(transactions)
+    transactionsRef.current = transactions
+    const displayTransactionToast = () => {
+        let interval = null
+        interval = setInterval(() => {
+            const currentTransactions = transactionsRef.current
+            currentTransactions
+                ? toast.custom(<TransactionNotification
+                    onMouseEntryHandler={handleViewportChange}
+                    transaction={getRandomTransaction(currentTransactions)}
+                />, {
+                    position: "top-left",
+                }
+                )
+                : null;
+        }, tickPeriod)
+        return () => clearInterval()
+    }
 
-  //resize
-  useEffect(() => {
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    useEffect(() => displayTransactionToast() , [tickPeriod])
 
-  const layers = useLayers({
-    transactionsFlow: transactionsFlow,
-    enabled: state,
-  });
+    const getRandomTransaction = (transactions) => {
+        if (transactions == null) return {};
+        const randIdx = Math.floor(Math.random() * transactions.length);
+        const txn = transactions[randIdx];
+        console.log("random txn", txn);
+        return txn;
+    };
 
-  window.setInterval(() => {
-    return transactions
-      ? toast.custom(
-          <TransactionNotification
-            onMouseEntryHandler={handleViewportChange}
-            transaction={getRandomTransaction()}
-          />,
-          {
-            position: "top-left",
-          }
-        )
-      : null;
-  }, 5000);
+    const checkboxes = (
+        <FormGroup row className={classes.checkboxes}>
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={state.arcs}
+                        onChange={handleCheck}
+                        name="arcs"
+                        color={colors.pistachioGreen}
+                        className={classes.checkbox}
+                    />
+                }
+                label="Arcs"
+            />
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={state.heatmap}
+                        onChange={handleCheck}
+                        name="heatmap"
+                        color={colors.pistachioGreen}
+                        className={classes.checkbox}
+                    />
+                }
+                label="Heatmap"
+            />
+        </FormGroup>
+    );
 
-  const getRandomTransaction = () => {
-    if (transactions == null) return {};
-    const randIdx = Math.floor(Math.random() * transactions.length);
-    const txn = transactions[randIdx];
-    console.log("random txn", txn);
-    return txn;
-  };
+    const loadedDisplay = (
+        <>
+            <div className={classes.container}>
+                <TopMenu/>
+                {checkboxes}
+            </div>
+            <MapContent viewport={viewport} layers={layers}/>
+        </>
+    );
 
-  const checkboxes = (
-    <FormGroup row className={classes.checkboxes}>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.arcs}
-            onChange={handleCheck}
-            name="arcs"
-            color="primary"
-            className={classes.checkbox}
-          />
-        }
-        label="Arcs"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.heatmap}
-            onChange={handleCheck}
-            name="heatmap"
-            color="primary"
-            className={classes.checkbox}
-          />
-        }
-        label="Heatmap"
-      />
-    </FormGroup>
-  );
-
-  const loadedDisplay = (
-    <>
-      <div className={classes.container}>
-        <TopMenu />
-        {checkboxes}
-      </div>
-      <MapContent viewport={viewport} layers={layers} />
-    </>
-  );
-
-  return (
-    <div className="App">
-      <Toaster />
-      {transactionsFlow === null ? (
-        <LoadingPage message={"nice"} />
-      ) : (
-        loadedDisplay
-      )}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Toaster/>
+            {transactionsFlow === null ? (
+                <LoadingPage message={"nice"}/>
+            ) : (
+                loadedDisplay
+            )}
+        </div>
+    );
 };
 
 export default App;
